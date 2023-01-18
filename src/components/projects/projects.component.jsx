c9e29915ddee1743c8cec328411e89a09b6c30e8
crosgar <cecilia.rossi.garcia@gmail.com>
2023-01-18 12:48:07 -0800

    menu in bw, projects page layout, small menu responsive

---
+++ src/components/projects/projects.component.jsx
@@ -0,0 +1,9 @@
+import './projects.styles.scss';
+
+export const ProjectsContent = (props) => {
+   return (
+      <div className='projects-content-container'>
+         projects
+      </div>
+   )
+}
--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,32 +1,37 @@
+import { useState } from 'react';
 import { MenuButton } from '../../components/menu-button/menu-button.component';
 import { useSelector } from "react-redux";
+import { ProjectsContent } from '../../components/projects/projects.component';
 import './home.styles.scss';
 
 const Home = () => {
    const { language } = useSelector((state) => state.language);
+   const { theme } = useSelector(state => state.theme);
+   const [content, setContent] = useState("large-menu");
 
    const handleMenuClick = (page) => {
-      console.log(page)
+      setContent(page);
    }
 
    return (
      <div className="home-page-container">
-       <div className="home-menu-container">
+       <div className="home-content-container">
          <MenuButton
            content={language === "eng" ? "ABOUT ME" : `SOBRE MI`}
-           className="cyan"
+           className={theme === "cmyk" ? `top cyan ${content}` : `top gray ${content}`}
            onClick={() => handleMenuClick("about")}
          />
          <MenuButton
            content={language === "eng" ? "PROJECTS" : `PROYECTOS`}
-           className="magenta"
+           className={theme === "cmyk" ? `middle magenta ${content}` : `middle dark-gray ${content}`}
            onClick={() => handleMenuClick("projects")}
          />
          <MenuButton
            content={language === "eng" ? "CONTACT" : `CONTACTO`}
-           className="yellow"
+           className={theme === "cmyk" ? `bottom yellow ${content}` : `bottom light-gray ${content}`}
            onClick={() => handleMenuClick("contact")}
          />
+         {content === "projects" && <ProjectsContent/>}
        </div>
      </div>
    );
