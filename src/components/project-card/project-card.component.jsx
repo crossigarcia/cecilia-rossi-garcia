9732898a67d498070a9542ec9b5d49fb8d9605b0
crosgar <cecilia.rossi.garcia@gmail.com>
2023-01-18 17:31:14 -0800

    project card updates to container styles

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -2,9 +2,18 @@ import './project-card.styles.scss';
 
 export const ProjectCard = (props) => {
    return (
-     <div className="project-card-container">
-      {/* <span className="project-card-overlay"></span> */}
-      <img src={require(`../../assets/imgs/${props.fileName}`)} alt={props.alt}/>
+     <div className='project-card-container'>
+         <div className='project-card-text-container'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quo, accusamus totam ex dolorem quos, laborum eveniet magni at possimus sapiente eligendi maxime perferendis provident! Quo itaque nemo quidem sed?
+         </div>
+         <div className="project-card-img-container">
+            <span className="project-card-overlay"></span>
+            <img
+            src={require(`../../assets/imgs/${props.fileName}`)}
+            alt={props.alt}
+            className="project-img"
+            />
+         </div>
      </div>
    );
 }
--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -4,7 +4,9 @@ import "./projects.styles.scss";
 export const ProjectsContent = (props) => {
   return (
     <div className="projects-content-container">
-      <ProjectCard fileName="karaok.gif" alt="GIF of the-kara-ok-corral website being used"/>
+      <ProjectCard fileName="karaok.gif" alt="GIF of 'the-kara-ok-corral' website being used"/>
+      <ProjectCard fileName="goodvice.gif" alt="GIF of 'The GoodVice Blog' website being used"/>
+      <ProjectCard fileName="dinner.gif" alt="GIF of 'What's for Dinner?' website being used"/>
     </div>
   );
 };
