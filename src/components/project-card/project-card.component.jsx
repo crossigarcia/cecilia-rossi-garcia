ce126f5532e5ab5e7c998ed0f6196a7703346199
crosgar <cecilia.rossi.garcia@gmail.com>
2023-01-19 18:19:49 -0800

    project cards expand and collapse logic in place

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -1,17 +1,28 @@
 import { useSelector } from "react-redux";
+import { RiCloseFill } from "react-icons/ri";
 import "./project-card.styles.scss";
 
 export const ProjectCard = ({ project, ...props }) => {
   const { theme } = useSelector((state) => state.theme);
 
   return (
-    <div className={`project-card-container ${project.className} ${theme}`}>
-      <div className={`project-card-text-container ${theme}`}>
-        <h2 className={`project-title`}>{project.title}<span className="project-date">{project.date}</span></h2>
+    <div id={props.id} className={`project-card-container ${props.className} ${theme}`}>
+      {props.className === 'open' && <button type="button" onClick={() => props.setOpenProject(null)} className="close-project"><RiCloseFill/></button>}
+      <div className={`project-card-text-container ${theme} ${props.className}`}>
+        <h2 className={`project-title`}>{project.title}</h2>
+        <span className="project-date">{project.date}</span>
         <p className="project-details">{project.details}</p>
+       {props.className !== "open" && <button type="button" name="more-project-info" onClick={() => props.setOpenProject(project)} className="more-info-btn">more info</button>}
       </div>
-      <div className="project-card-img-container">
-        <span className={`project-card-overlay ${theme}`}></span>
+      {props.className === 'open' && (
+         <div className="project-additionals">
+            <span>Tech Stack: {project.techStack.join(", ")}</span>
+            <span>Github: {project.github}</span>
+            <span>Deployed: {project.deployed}</span>
+         </div>
+      )}
+      <div className={`project-card-img-container ${props.className}`}>
+        <span className={`project-card-overlay ${props.className} ${theme}`}></span>
         <img
           src={require(`../../assets/imgs/${project.fileName}`)}
           alt={project.alt}
--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { projectCardData } from '../../assets/constants/data';
 import { ProjectCard } from "../project-card/project-card.component";
 import "./projects.styles.scss";
 
 export const ProjectsContent = (props) => {
-   const [openProject, setOpenProject] = useState(0);
-   
+   const [openProject, setOpenProject] = useState(null);
+
+   useEffect(() => {
+      if (openProject) {
+         const el = document.getElementById(openProject.id);
+         el.parentNode.scrollTop = el.offsetTop - el.parentNode.offsetTop;
+      }
+   }, [openProject])
+
   return (
     <div className="projects-content-container">
-      {projectCardData.map((project) => (
-        <ProjectCard key={project.id} className={openProject === project.id ? 'open' : ''} project={project}/>
-      ))}
+      { projectCardData.map((project) => (
+          <ProjectCard
+            id={project.id}
+            key={project.id}
+            project={project}
+            setOpenProject={setOpenProject}
+            className={(openProject && openProject.id === project.id) ? "open" : ""}
+          />
+        ))}
     </div>
   );
 };
