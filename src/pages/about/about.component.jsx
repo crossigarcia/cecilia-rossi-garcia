aeca0d98c9a654b607a04d751a56691e20b38717
crosgar <cecilia.rossi.garcia@gmail.com>
2023-01-25 16:48:13 -0800

    about page dispatch content

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -1,12 +1,25 @@
-import './about.styles.scss';
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setTitle } from "../../redux/features/nav-title/navTitleSlice";
+import { setContent } from "../../redux/features/page-content/pageContentSlice";
+import "./about.styles.scss";
 
 export const AboutContent = () => {
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+     dispatch(setTitle(true));
+     dispatch(setContent("about"));
+   }, [dispatch]);
+
    return (
-      <div className='about-content-container'>
-         <div className='about-main-text-container'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident autem explicabo, exercitationem, fuga perferendis voluptatum inventore vitae tenetur quaerat alias veritatis! Nihil ipsum iure natus veniam aut. Tempore, rerum voluptatibus.
+      <div className="about-content-container">
+         <div className="about-main-text-container">
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident autem
+         explicabo, exercitationem, fuga perferendis voluptatum inventore vitae
+         tenetur quaerat alias veritatis! Nihil ipsum iure natus veniam aut.
+         Tempore, rerum voluptatibus.
          </div>
       </div>
-   )
-}
-
+   );
+};
--- src/pages/navigation/navigation.component.jsx
+++ src/pages/navigation/navigation.component.jsx
@@ -1,9 +1,7 @@
-import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setTheme } from "../../redux/features/theme/themeSlice";
 import { setLanguage } from "../../redux/features/language/languageSlice";
-import { setContent } from "../../redux/features/page-content/pageContentSlice";
 import { Switch } from "antd";
 import { MenuButton } from "../../components/menu-button/menu-button.component";
 import "./navigation.styles.scss";
@@ -17,14 +15,8 @@ const Nav = () => {
   const { content } = useSelector((state) => state.content);
 
   const handleMenuClick = (page) => {
-    dispatch(setContent(page));
+    navigate(`/${page}`);
   };
-
-  useEffect(() => {
-    if (content !== "large-menu") {
-      navigate(`/${content}`);
-    } 
-  }, [content, navigate]);
 
   const handleThemeChange = (value) => {
     dispatch(setTheme(value === true ? "bw" : "cmyk"));
@@ -59,35 +51,31 @@ const Nav = () => {
           className={`theme-switch ${theme}`}
         />
       </div>
-      {title && (
-        <>
-          <MenuButton
-            content={language === "eng" ? "ABOUT ME" : `SOBRE MI`}
-            className={
-              theme === "cmyk" ? `top cyan ${content}` : `top gray ${content}`
-            }
-            onClick={() => handleMenuClick("about")}
-          />
-          <MenuButton
-            content={language === "eng" ? "PROJECTS" : `PROYECTOS`}
-            className={
-              theme === "cmyk"
-                ? `middle magenta ${content}`
-                : `middle dark-gray ${content}`
-            }
-            onClick={() => handleMenuClick("projects")}
-          />
-          <MenuButton
-            content={language === "eng" ? "CONTACT" : `CONTACTO`}
-            className={
-              theme === "cmyk"
-                ? `bottom yellow ${content}`
-                : `bottom light-gray ${content}`
-            }
-            onClick={() => handleMenuClick("contact")}
-          />
-        </>
-      )}
+      <MenuButton
+        content={language === "eng" ? "ABOUT ME" : `SOBRE MI`}
+        className={
+          theme === "cmyk" ? `top cyan ${content}` : `top gray ${content}`
+        }
+        onClick={() => handleMenuClick("about")}
+      />
+      <MenuButton
+        content={language === "eng" ? "PROJECTS" : `PROYECTOS`}
+        className={
+          theme === "cmyk"
+            ? `middle magenta ${content}`
+            : `middle dark-gray ${content}`
+        }
+        onClick={() => handleMenuClick("projects")}
+      />
+      <MenuButton
+        content={language === "eng" ? "CONTACT" : `CONTACTO`}
+        className={
+          theme === "cmyk"
+            ? `bottom yellow ${content}`
+            : `bottom light-gray ${content}`
+        }
+        onClick={() => handleMenuClick("contact")}
+      />
       <Outlet />
     </nav>
   );
