62dc53dc37097d0377ed9e03c681249e396d5736
crosgar <cecilia.rossi.garcia@gmail.com>
2023-02-01 13:57:59 -0800

    skill list cubes

---
+++ src/components/skills-list/skills-list.component.jsx
@@ -0,0 +1,16 @@
+import './skills-list.styles.scss';
+
+export const SkillsList = ({ list, title, ...props}) => {
+   return (
+     <div className={`skill-list-container ${props.className}`}>
+       <h3 className="skill-header">{title} Skills</h3>
+       <div className={`wave ${props.className}`}>
+         {list.map((item) => (
+           <span className={`list-item ${props.className}`} key={item}>
+             {item}
+           </span>
+         ))}
+       </div>
+     </div>
+   );
+}
--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -1,86 +1,29 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setTitle } from "../../redux/features/nav-title/navTitleSlice";
 import { setContent } from "../../redux/features/page-content/pageContentSlice";
 import { aboutData } from "../../assets/constants/data";
+import { SkillsList } from "../../components/skills-list/skills-list.component";
 import "./about.styles.scss";
 
 export const AboutContent = () => {
    const dispatch = useDispatch();
    const { theme } = useSelector(state => state.theme);
-   const [openSkills, setOpenSkills] = useState({ primary: true, secondary: false, additional: false });
 
    useEffect(() => {
      dispatch(setTitle(true));
      dispatch(setContent("about"));
    }, [dispatch]);
 
-   const handleClick = (name) => {
-      setOpenSkills({...openSkills, [name]: !openSkills[name] });
-   }
-   
-
    return (
      <div className="about-content-container">
        <div className={`about-main-container ${theme}`}>
          <p className={`about-main-text ${theme}`}>{aboutData.main}</p>
        </div>
-       <div className={`skills-container ${theme}`}>
-         {openSkills["primary"] && (
-           <div className={`skill-list ${openSkills["primary"] ? "open" : ""}`}>
-             {aboutData.primary.map((skill, index) => (
-               <span className={`skill ${theme}`} key={index}>
-                 {skill}
-               </span>
-             ))}
-           </div>
-         )}
-         <span
-           role="button"
-           aria-label="button to open or close primary skills tab"
-           onClick={() => handleClick("primary")}
-           className={`skill-btn ${theme} ${openSkills["primary"] ? "open" : ""}`}
-         >
-           Primary
-         </span>
-       </div>
-       <div className={`skills-container ${theme}`}>
-         {openSkills["secondary"] && (
-           <div className="skill-list">
-             {aboutData.secondary.map((skill, index) => (
-               <span className={`skill ${theme}`} key={index}>
-                 {skill}
-               </span>
-             ))}
-           </div>
-         )}
-         <span
-           role="button"
-           aria-label="button to open or close secondary skills tab"
-           onClick={() => handleClick("secondary")}
-           className={`skill-btn ${theme}`}
-         >
-           Secondary
-         </span>
-       </div>
-       <div className={`skills-container ${theme}`}>
-         {openSkills["additional"] && (
-           <div className="skill-list">
-             {aboutData.additional.map((skill, index) => (
-               <span className={`skill ${theme}`} key={index}>
-                 {skill}
-               </span>
-             ))}
-           </div>
-         )}
-         <span
-           role="button"
-           aria-label="button to open or close additional skills tab"
-           onClick={() => handleClick("additional")}
-           className={`skill-btn ${theme}`}
-         >
-           Additional
-         </span>
+       <div className={`skills-container`}>
+         <SkillsList list={aboutData.primary} title="Primary" className={theme === 'cmyk' ? `cyan` : `gray`}/>
+         <SkillsList list={aboutData.secondary} title="Secondary" className={theme === 'cmyk' ? `magenta` : `dark-gray`}/>
+         <SkillsList list={aboutData.additional} title="Additional" className={theme === 'cmyk' ? `yellow` : `light-gray`}/>
        </div>
      </div>
    );
