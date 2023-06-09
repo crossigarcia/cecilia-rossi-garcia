f73a601ef60885861f48a1f073d28127f8e039e2
crosgar <cecilia.rossi.garcia@gmail.com>
2023-06-08 19:49:49 -0500

    remove hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import App from './App';
@@ -11,9 +11,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <HashRouter basename="/">
           <App />
-        </HashRouter>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
